022275dbda4d13a56742a1194dda6c8d44316d3a
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-06 14:24:24 +0800

    feat: added knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,5 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
 
 configure(require.context("../src", true, /\.stories\.(js|mdx)$/), module);
+addDecorator(withKnobs);
