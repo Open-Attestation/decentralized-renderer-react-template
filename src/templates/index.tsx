70b8080b08b13b93a58f74bfe9c433b6a5d5991c
Nebulis <nebounet@gmail.com>
2020-02-27 13:51:18 +0800

    chore: upgrade @govtechsg/decentralized-renderer-react-components to 2.1.0

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -4,7 +4,6 @@ import styled from "@emotion/styled";
 import { CustomTemplate } from "./customTemplate/customTemplate";
 
 export const registry: TemplateRegistry<any> = {
-  default: templates,
   custom: templates,
   red: [
     {
