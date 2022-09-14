4a1fa281eba214ced88ea7c676bc38ea999d465d
Sim Boon Long <simboonlong@gmail.com>
2022-09-14 14:27:02 +0800

    fix: separate e2e

--- e2e/integration/integration.spec.ts
+++ e2e/integration/integration.spec.ts
@@ -1,12 +1,6 @@
 import { Selector } from "testcafe";
-import { CustomTemplateCertificate, customTemplateCertificate } from "../templates/samples";
-import percySnapshot from "@percy/testcafe";
+import * as document from "../fixtures/document.json";
 
-// assign certificate to a variable otherwise there is an error ... ReferenceError: sample_1 is not defined
-const document: CustomTemplateCertificate = {
-  ...customTemplateCertificate,
-  $template: { ...customTemplateCertificate.$template, name: "red" }
-};
 fixture("Custom Red Certificate Template").page`http://localhost:3000`;
 
 const CustomTemplate = Selector("#custom-template");
@@ -32,7 +26,4 @@ test("Custom certificate is rendered correctly", async test => {
   // test the title is displayed
   await test.expect(CustomTemplate.visible).ok();
   await test.expect(CustomTemplate.textContent).contains("Bar is awesome");
-
-  // take a snapshot
-  await percySnapshot(test, "Rendered custom template");
 });
